41a225c146b3f53a375a645c162b6e486292ce8d
Jboy Abecia <jboyabecia@gmail.com>
2025-09-01 00:28:20 +0800

    Continuation on the release section

---
+++ src/pages/dashboard/constants.jsx
@@ -0,0 +1,18 @@
+export const NOTIFICATION_TYPE = {
+  ERROR: "Error",
+  SUCCESS: "Success",
+};
+
+export const NOTIFICATION_COLOR = {
+  RED: "red",
+  GREEN: "green",
+  BLUE: "blue",
+};
+
+export const FORM_ERROR_MESSAGES = {
+  NEW_RELEASED_DOCUMENT_SUCCESS: "New released document added successfully.",
+  INVALID_FORMAT:
+    "You are trying to attach a non-PDF format. Please check and attach again.",
+  REQUIRED_EMPTY:
+    "Required fields are not allowed to be empty. Please review the empty fields.",
+};
